a5525c091353feaf4e34d05f13144ce14d663109
CodeArgent <andreprata2000@outlook.com>
2023-12-04 08:56:57 +0000

    Add aoc-ts 2023 day 1

---
+++ 2023/1/index.ts
@@ -0,0 +1,23 @@
+import { data } from "./data";
+
+type myArray = Array<Array<string>>;
+const manipulatedData: myArray = [];
+
+for (const [key, value] of Object.entries(data)) {
+  const numberKey = parseInt(key);
+  manipulatedData[numberKey] = [];
+
+  for (let digit of value) {
+    if (parseInt(digit)) {
+      manipulatedData[numberKey].push(digit);
+    }
+  }
+}
+
+let counter = 0;
+manipulatedData.forEach(element => {
+  const length = element.length - 1;
+  counter += +(element[0] + element[length]);
+});
+
+console.log(counter);
